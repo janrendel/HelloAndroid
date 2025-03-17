b51cb9956d705c72f42db5a3ec848bd1ce56466b
janrendel <janrendel11@gmail.com>
2025-03-17 16:29:05 +0900

    EditText에 입력 받아서 처리 결과표시

---
+++ app/src/main/java/kr/ac/kopo/helloandroid/MainActivity.java
@@ -0,0 +1,45 @@
+package kr.ac.kopo.helloandroid;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher_foreground);
+        Button btn1 = findViewById(R.id.btn1);
+        TextView text1 = findViewById(R.id.textView2);
+        EditText edit1 = findViewById(R.id.editName);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(getApplicationContext(), "버튼을 클릭하셨네요~", Toast.LENGTH_LONG);
+                toast.show();
+                String name = edit1.getText().toString();
+                text1.setText(name + "입니다.");
+            }
+        });
+
+//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+//            return insets;
+//        });
+    }
+}
